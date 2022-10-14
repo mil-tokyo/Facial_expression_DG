18317838393a809fe3075a0c9d7e5829447103e0
Yusuke Kurose <kurose@mi.t.u-tokyo.ac.jp>
2022-10-14 18:19:51 +0900

    Add GUI

--- gui/gui.py
+++ gui/gui.py
@@ -72,6 +72,23 @@ class VideoView(wx.StaticBitmap):
         dc.Clear()
         dc.DrawBitmap(self.image, 0, 0, True)
 
+class Result_graph(wx.Panel):
+    def __init__(self, parent, id):
+        super(Result_graph, self).__init__(parent, id, pos=(360, 0), size=(120, 120))
+        self.SetBackgroundColour('WHITE')
+        self.Bind(wx.EVT_PAINT, self.OnPaint)
+
+    def OnPaint(self, event):
+        dc = wx.PaintDC(self)
+        dc.SetPen(wx.Pen('blue'))
+        dc.SetBrush(wx.Brush('blue'))
+        dc.DrawRectangle(20, 20, 260, 40)
+        dc.SetPen(wx.Pen('yellow'))
+        dc.SetBrush(wx.Brush('yellow'))
+        dc.DrawRectangle(20, 60, 200, 40)
+        dc.SetPen(wx.Pen('red'))
+        dc.SetBrush(wx.Brush('red'))
+        dc.DrawRectangle(20, 100, 160, 40)
 
 
 class MyFrame(wx.Frame):
@@ -79,14 +96,16 @@ class MyFrame(wx.Frame):
         super(MyFrame, self).__init__(parent=parent, title=title)
         self.Bind(wx.EVT_CLOSE, self.frame_close)
         self.stopFlag = False
-
+        self.image_size = (320, 240)
         self.demo = FacialExpressionDemo(model_name=model_name, train=train, num_class=7, num_domains=num_domains, device=0, path=path)
         panel = wx.Panel(self, wx.ID_ANY)
-        img = wx.Image(640, 480)
+        img = wx.Image(self.image_size[0], self.image_size[1])
         self.video_frame = VideoView(panel, wx.ID_ANY, wx.Bitmap(img))
+        self.graph = Result_graph(self, wx.ID_ANY)
 
         sizer = wx.BoxSizer(wx.VERTICAL)
         sizer.Add(self.video_frame)
+        self.Fit()
 
         # # Read 1st frame
         # ret, image = cap.read()
@@ -135,10 +154,11 @@ class MyFrame(wx.Frame):
         # updateFrame()
 
     def create_wx_bitmap_from_cv2_image(self, cv2_image):
+        cv2_image = cv2.flip(cv2_image, 1)
         cv2_rgb_image = cv2.cvtColor(cv2_image, cv2.COLOR_BGR2RGB)
-        resize_image = cv2.resize(cv2_rgb_image, (640, 480), cv2.INTER_LINEAR)
+        resize_image = cv2.resize(cv2_rgb_image, self.image_size, cv2.INTER_LINEAR)
         output = self.demo.evaluation(resize_image)
-        return wx.Bitmap.FromBuffer(640, 480, resize_image)
+        return wx.Bitmap.FromBuffer(self.image_size[0], self.image_size[1], resize_image)
 
     def frame_close(self, event):
         self.stopFlag = True
