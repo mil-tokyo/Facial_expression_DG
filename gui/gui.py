f4d3a8c1750124ebd25f441a81e0a672e8824720
Yusuke Kurose <kurose@mi.t.u-tokyo.ac.jp>
2022-10-14 14:03:26 +0900

    add gui

--- gui/gui.py
+++ gui/gui.py
@@ -7,7 +7,7 @@ cap = cv2.VideoCapture(0)
 
 class MyFrame(wx.Frame):
     def __init__(self, parent, title):
-        super(MyFrame, self).__init__(self, parent, title)
+        super(MyFrame, self).__init__(parent=parent, title=title)
         self.Bind(wx.EVT_CLOSE, self.frame_close)
         self.stopFlag = False
 
@@ -15,7 +15,7 @@ class MyFrame(wx.Frame):
         ret, image = cap.read()
         image = cv2.flip(image, 1)
         cv2image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
+        print(cv2image.shape)
         self.wximage = wx.Image(cv2image.shape[1], cv2image.shape[0], cv2image)
         bitmap = self.wximage.ConvertToBitmap()
         self.stbmp = wx.StaticBitmap(self, -1, bitmap, (0, 0), self.GetClientSize())
@@ -45,4 +45,7 @@ class MyFrame(wx.Frame):
 if __name__ == '__main__':
     app = wx.App()
     frame = MyFrame(None, 'Facial_expression_prediction')
+    #frame = wx.Frame(parent=None, id=-1, title="wxPython", size=(400, 400))
+    frame.Show()
+    #app.SetTopWindow(frame)
     app.MainLoop()
